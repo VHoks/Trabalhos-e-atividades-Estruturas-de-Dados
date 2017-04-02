9227b4fd499a36c4ae2179dd88b9b8227f0293a5
VHoks <vitorhokino@hotmail.com>
2017-04-02 00:11:00 -0300

    Add files via upload

--- Atividade aula 7.c
+++ Atividade aula 7.c
@@ -1,17 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+#define TAM_MAX 10
+
+int stack[TAM_MAX];
+short int top = -1;
+
+int vazio(void)
+{
+    if(top == -1)
+        return(1);
+    else
+        return(0);
+}
+
+int cheio(void)
+{
+    if(top >= TAM_MAX)
+        return(1);
+    else
+        return(0);
+}
+
+int push(int n)
+{
+    if(!cheio())
+    {
+        ++top;
+        stack[top] = n;
+    }
+    else
+    {
+        printf("A memoria da pilha esta lotada!\n\n");
+        exit(1);
+    }
+}
+
+int pop(void)
+{
+    int n;
+
+    if(!vazio())
+    {
+        n = stack[top];
+        --top;
+        return(n);
+    }
+    else
+    {
+        printf("A memoria da pilha esta vazia!\n\n");
+        exit(1);
+    }
+}
 
 int main(void)
 {
+    int num;
     short int i;
-    unsigned int tam;
-    
-    do{
-    printf("\nInforme o tamanho da pilha (max 15): ");
-    scanf("%u", &tam);
-    }while(tam < 0 || tam > 15);
-    
-    int *pilha = (int *)malloc(tam * sizeof(int));
-    
-    for(i = 0; i
+    srand((unsigned)time(NULL));
+
+    printf("Este programa empilha 10 numeros aleatorios e os imprime enquanto os desempilha.\n");
+
+    for(i = 0; i < TAM_MAX; ++i)
+    {
+        num = (rand() % 101) + 1;
+        push(num);
+        printf("%do numero: %d\n", i + 1, num);
+    }
+
+    printf("\nPilha:\n");
+
+    while(!vazio())
+    {
+        num = pop();
+        printf("%d\n", num);
+    }
+
+    printf("\nPilha vazia: %s", vazio()? "SIM" : "NAO");
+
+    return(0);
 }
