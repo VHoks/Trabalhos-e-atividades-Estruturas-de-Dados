6a3c8ff9229f4b618d95f3534de5b39a4a47d315
VHoks <vitorhokino@hotmail.com>
2017-04-03 04:56:59 -0300

    Add files via upload

--- Trabalho 1.c
+++ Trabalho 1.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-/* Diretivas de pre-processamento para função que limpa o console */
+/* Diretivas de pre-processamento para fun��o que limpa o console */
 #if defined(__MINGW32__) || defined(_MSC_VER)
 #define limpar_tela() system("cls")
 #else
@@ -11,75 +12,194 @@
 /* Macros dos tamanhos da matriz e dos nomes dos jogadores*/
 #define TAM_MAPA 15
 #define TAM_NOME 30
+#define GANHOU 14
 
-/* Estrutura modelo dos navios */
-typedef struct ponto{
-    short int x; /* Coordenada 'X' da parte do navio */
-    short int y; /* Coordenada 'Y' da parte do navio */
-    short int status; /* Variavel que guarda o status da parte do navio (operante = 0 ou destruida = 1) */
-}navio;
+void montar_mapas(char[][TAM_MAPA], char[][TAM_MAPA], char[][TAM_MAPA], char[][TAM_MAPA]);
+void posicionar_navio(char[][TAM_MAPA], short int, char);
+void imprimir_instrucoes(void);
+void obter_nomes(char[], char[]);
+void imprimir_mapa(char[][TAM_MAPA]);
+void jogada_ant_p1(short int *, short int *, short int *, int);
+void jogada_ant_p2(short int *, short int *, short int *, int);
+int jogada(char[][TAM_MAPA], char[][TAM_MAPA], char[TAM_NOME],int *, short int *, short int *, short int *);
 
-/* Variaveis globais */
-unsigned int pontos_p1 = 0, pontos_p2 = 0; /* Pontos dos jogadores */
+int main(void)
+{
+    char mostrar_mapa_p1[TAM_MAPA][TAM_MAPA], mostrar_mapa_p2[TAM_MAPA][TAM_MAPA]; /* Mapas que ser�o mostrados aos dos jogadores */
+    char nav_mapa_p1[TAM_MAPA][TAM_MAPA], nav_mapa_p2[TAM_MAPA][TAM_MAPA]; /* Mapas que contem os navios */
+    short int resultado_p1, resultado_p2; /* Variaveis usadas para armazenar o valor que indica se o jogador acertou ou errou a jogada */
+    short int x_p1 = -1, y_p1 = -1, x_p2 = -1, y_p2 = -1; /* Variaveis que armazenam as coordenadas fornecidas pelos jogadores a cada jogada para serem informadas na proxima jogada */
+    int pontos_p1 = 0, pontos_p2 = 0; /* Pontos dos jogadores */
+    short int acertos_p1 = 0, acertos_p2 = 0; /* Quantidade de navios destruidos por cada jogador */
+    char nome_p1[TAM_NOME], nome_p2[TAM_NOME]; /* Nome dos jogadores */
 
-short int nav_destruidos_mapa1 = 0, nav_destruidos_p2 = 0; /* Quantidade de navios destruidos por cada jogador */
+    montar_mapas(mostrar_mapa_p1, mostrar_mapa_p2, nav_mapa_p1, nav_mapa_p2);
 
-char nome_p1[TAM_NOME], nome_p2[TAM_NOME]; /* Nome dos jogadores */
+    posicionar_navio(nav_mapa_p1, 5, 'P');
+    posicionar_navio(nav_mapa_p1, 4, 'E');
+    posicionar_navio(nav_mapa_p1, 3, 'C');
+    posicionar_navio(nav_mapa_p1, 2, 'S');
 
-char mapa_p1[TAM_MAPA][TAM_MAPA], mapa_p2[TAM_MAPA][TAM_MAPA]; /* Mapa dos jogadores */
+    posicionar_navio(nav_mapa_p2, 5, 'P');
+    posicionar_navio(nav_mapa_p2, 4, 'E');
+    posicionar_navio(nav_mapa_p2, 3, 'C');
+    posicionar_navio(nav_mapa_p2, 2, 'S');
 
-navio porta_aviao_mapa1[5], encouracado_mapa1[4], contratorpedeiro_mapa1[3], submarino_mapa1[2], porta_aviao_mapa2[5], encouracado_mapa2[4], contratorpedeiro_mapa2[3], submarino_mapa2[2]; /* Vetores de estruturas que correspondem aos navios */
+    imprimir_instrucoes();
 
-void montar_mapas(void)
+    obter_nomes(nome_p1, nome_p2);
+
+    do{
+        jogada_ant_p1(&x_p1, &y_p1, &resultado_p1, pontos_p1);
+        resultado_p1 = jogada(mostrar_mapa_p1, nav_mapa_p1, nome_p1, &pontos_p1, &acertos_p1, &x_p1, &y_p1); /* Vez do p1 */
+        printf("Pressione ENTER para continuar...");
+        getchar();
+        getchar();
+        limpar_tela();
+
+        jogada_ant_p2(&x_p2, &y_p2, &resultado_p2, pontos_p2);
+        resultado_p2 = jogada(mostrar_mapa_p2, nav_mapa_p2, nome_p2, &pontos_p2, &acertos_p2, &x_p2, &y_p2); /* Vez do p2 */
+        printf("Pressione ENTER para continuar...");
+        getchar();
+        getchar();
+        limpar_tela();
+    }while(acertos_p1 < GANHOU && acertos_p2 < GANHOU);
+
+    if(acertos_p1 > acertos_p2)
+        printf("Parabens Almirante %s, o senhor destruiu a esquadra inimiga!\nPONTUACAO FINAL: %d", nome_p1, pontos_p1);
+    else if(acertos_p1 < acertos_p2)
+        printf("Parabens Almirante %s, o senhor destruiu a esquadra inimiga!\nPONTUACAO FINAL: %d", nome_p2, pontos_p2);
+    else
+        printf("EMPATE! Ambas esquadras foram destruidas.");
+    return(0);
+}
+
+void montar_mapas(char mostrar_mapa_p1[][TAM_MAPA], char mostrar_mapa_p2[][TAM_MAPA], char nav_mapa_p1[][TAM_MAPA], char nav_mapa_p2[][TAM_MAPA])
 {
     short int i, j;
-    
+
     for(i = 0; i < TAM_MAPA; ++i)
         for(j = 0; j < TAM_MAPA; ++j)
         {
-            mapa_p1[i][j] = '~';
-            mapa_p2[i][j] = '~';
+            mostrar_mapa_p1[i][j] = '~';
+            mostrar_mapa_p2[i][j] = '~';
+            nav_mapa_p1[i][j] = '~';
+            nav_mapa_p2[i][j] = '~';
         }
 }
 
-void preparar_navios(void)
+void posicionar_navio(char mapa[][TAM_MAPA], short int tam_navio, char navio)
 {
-    short int i;
-    
-    for(i = 0; i < 5; ++i)
-        porta_aviao.status = 0
+    /* Randomizacao */
+    time_t t;
+    srand((unsigned)time(&t));
+
+    short int j, ran_x, ran_y, check;
+
+    do{
+        check = 0;
+        ran_x = rand() % (TAM_MAPA - tam_navio);
+        ran_y = rand() % TAM_MAPA;
+        for(j = 0; j < tam_navio && mapa[ran_y][j + ran_x] == '~'; ++j, ++check)
+            mapa[ran_y][j + ran_x] = navio;
+    }while(check != tam_navio);
 }
 
 void imprimir_instrucoes(void)
 {
     printf("\nBem vindo ao jogo Batalha Naval!");
-    printf("\nO campo de batalha tem dimensão de 15 X 15 espaços.");
-    printf("\nCada jogador tem um de cada dos seguintes navios:\nPorta-aviao - 5 espaços\nEncouracado - 4 espaços\nContratorpedeiro - 3 espaços\nSubmarino - 2 espaços\n");
+    printf("\nO campo de batalha tem dimensao de %d X %d unidades.", TAM_MAPA, TAM_MAPA);
+    printf("\nCada jogador tem um de cada dos seguintes navios:\nPorta-aviao - 5 espacos\nEncouracado - 4 espacos\nContratorpedeiro - 3 espacos\nSubmarino - 2 espacos\n");
     printf("\nVence quem derrubar todos os navios inimigos primeiro");
 }
 
-void obter_nomes(void)
+void obter_nomes(char nome_p1[TAM_NOME], char nome_p2[TAM_NOME])
 {
-    printf("\n\nNome do jogador 1: ");
-    fgets(nome_p1, TAM_NOME, stdin);
-    printf("\nNome do jogador 2: ");
-    fgets(nome_p2, TAM_NOME, stdin);
+    printf("\n\nNome do jogador 1 (max 30 caracteres): ");
+    scanf("%[^\n]", nome_p1);
+    getchar();
+    printf("Nome do jogador 2 (max 30 caracteres): ");
+    scanf("%[^\n]", nome_p2);
+    getchar();
+    printf("\n\n");
 }
 
-int jogo()
+void imprimir_mapa(char mapa[][TAM_MAPA])
+{
+    short int i, j;
+
+    for(j = 0; j < TAM_MAPA; ++j)
+        printf("%d\t", j);
+    printf("\n\n");
+    for(i = 0; i < TAM_MAPA; ++i)
+    {
+        for(j = 0; j < TAM_MAPA; ++j)
+            printf("%c\t", mapa[i][j]);
+        printf("%d\n\n", i);
+    }
+}
+
+void jogada_ant_p1(short int *x, short int *y, short int *resultado, int pontos)
+{
+    if(*x != -1 && *y != -1)
+    {
+        printf("Sua pontuacao: %d\n", pontos);
+        if(*resultado == 0)
+            printf("Jogada anterior: Acerto na coordenada (%d, %d)\n\n", *x, *y);
+        else
+            printf("Jogada anterior: Tiro ao mar na coordenada (%d, %d)\n\n", *x, *y);
+    }
+
+}
+
+void jogada_ant_p2(short int *x, short int *y, short int *resultado, int pontos)
+{
+    if(*x != -1 && *y != -1)
+    {
+        printf("Sua pontuacao: %d\n", pontos);
+        if(*resultado == 0)
+            printf("Jogada anterior: Acerto na coordenada (%d, %d)\n\n", *x, *y);
+        else
+            printf("Jogada anterior: Tiro ao mar na coordenada (%d, %d)\n\n", *x, *y);
+    }
+}
+
+int jogada(char mostrar_mapa[][TAM_MAPA], char nav_mapa[][TAM_MAPA], char nome[TAM_NOME],int *pontos, short int *acertos, short int *x_p, short int *y_p)
 {
     int x, y;
-}
 
-int main(void)
-{    
-    montar_mapas();
-    preparar_navios();
-    imprimir_instrucoes();
-    obter_nomes();
-    
+    imprimir_mapa(mostrar_mapa);
+
     do{
-        jogo();
-    }while();
-    
+        printf("\nAlmirante %s, escolha uma coordenada (X, Y) para atacar!\n", nome);
+        printf("X: ");
+        scanf("%d", &x);
+        printf("Y: ");
+        scanf("%d", &y);
+        if(x < 0 || x >= TAM_MAPA || y < 0 || y >= TAM_MAPA)
+            printf("\nCoordenada invalida!\n\n");
+        if(nav_mapa[x][y] == '*')
+            printf("\nEsta coordenada ja recebeu um ataque!\n\n");
+    }while(x < 0 || x >= TAM_MAPA || y < 0 || y >= TAM_MAPA || nav_mapa[x][y] == '*');
+
+    *x_p = x;
+    *y_p = y;
+
+    if(nav_mapa[x][y] != '~')
+    {
+        printf("\nACERTO!\n\n");
+        mostrar_mapa[x][y] = nav_mapa[x][y];
+        nav_mapa[x][y] = '*';
+        *pontos += 1000;
+        *acertos += 1;
+        return(0);
+    }
+    else
+    {
+        printf("\nTiro ao mar...\n\n");
+        nav_mapa[x][y] = '*';
+        mostrar_mapa[x][y] = '*';
+        *pontos -= 100;
+        return(1);
+    }
 }
