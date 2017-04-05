51aec29c0be29b2769c84b898f568fd3f9482447
VHoks <vitorhokino@hotmail.com>
2017-04-05 20:51:30 -0300

    Add files via upload

--- Trabalho 1.c
+++ Trabalho 1.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-/* Diretivas de pre-processamento para funÁ„o que limpa o console */
+/* Diretivas de pre-processamento para fun√ß√£o que limpa o console */
 #if defined(__MINGW32__) || defined(_MSC_VER)
 #define limpar_tela() system("cls")
 #else
@@ -12,10 +12,10 @@
 /* Macros dos tamanhos da matriz e dos nomes dos jogadores*/
 #define TAM_MAPA 15
 #define TAM_NOME 30
-#define GANHOU 14
+#define GANHOU 25
 
 void montar_mapas(char[][TAM_MAPA], char[][TAM_MAPA], char[][TAM_MAPA], char[][TAM_MAPA]);
-void posicionar_navio(char[][TAM_MAPA], short int, char);
+void posicionar_navio(char[][TAM_MAPA], short int, char, unsigned int);
 void imprimir_instrucoes(void);
 void obter_nomes(char[], char[]);
 void imprimir_mapa(char[][TAM_MAPA]);
@@ -25,26 +25,55 @@ int jogada(char[][TAM_MAPA], char[][TAM_MAPA], char[TAM_NOME],int *, short int *
 
 int main(void)
 {
-    char mostrar_mapa_p1[TAM_MAPA][TAM_MAPA], mostrar_mapa_p2[TAM_MAPA][TAM_MAPA]; /* Mapas que ser„o mostrados aos dos jogadores */
+    char mostrar_mapa_p1[TAM_MAPA][TAM_MAPA], mostrar_mapa_p2[TAM_MAPA][TAM_MAPA]; /* Mapas que ser√£o mostrados aos dos jogadores */
     char nav_mapa_p1[TAM_MAPA][TAM_MAPA], nav_mapa_p2[TAM_MAPA][TAM_MAPA]; /* Mapas que contem os navios */
     short int resultado_p1, resultado_p2; /* Variaveis usadas para armazenar o valor que indica se o jogador acertou ou errou a jogada */
     short int x_p1 = -1, y_p1 = -1, x_p2 = -1, y_p2 = -1; /* Variaveis que armazenam as coordenadas fornecidas pelos jogadores a cada jogada para serem informadas na proxima jogada */
-    int pontos_p1 = 0, pontos_p2 = 0; /* Pontos dos jogadores */
+    int seed1, pontos_p1 = 0, pontos_p2 = 0; /* Pontos dos jogadores */
     short int acertos_p1 = 0, acertos_p2 = 0; /* Quantidade de navios destruidos por cada jogador */
     char nome_p1[TAM_NOME], nome_p2[TAM_NOME]; /* Nome dos jogadores */
 
+    srand((unsigned)time(NULL));
+
     montar_mapas(mostrar_mapa_p1, mostrar_mapa_p2, nav_mapa_p1, nav_mapa_p2);
 
-    posicionar_navio(nav_mapa_p1, 5, 'P');
-    posicionar_navio(nav_mapa_p1, 4, 'E');
-    posicionar_navio(nav_mapa_p1, 3, 'C');
-    posicionar_navio(nav_mapa_p1, 2, 'S');
+    seed1 = rand();
+    posicionar_navio(nav_mapa_p1, 5, 'P', seed1);
+    seed1 = rand();
+    posicionar_navio(nav_mapa_p1, 4, 'E', seed1);
+    seed1 = rand();
+    posicionar_navio(nav_mapa_p1, 4, 'E', seed1);
+    seed1 = rand();
+    posicionar_navio(nav_mapa_p1, 3, 'C', seed1);
+    seed1 = rand();
+    posicionar_navio(nav_mapa_p1, 3, 'C', seed1);
+    seed1 = rand();
+    posicionar_navio(nav_mapa_p1, 2, 'S', seed1);
+    seed1 = rand();
+    posicionar_navio(nav_mapa_p1, 2, 'S', seed1);
+    seed1 = rand();
+    posicionar_navio(nav_mapa_p1, 2, 'S', seed1);
 
-    posicionar_navio(nav_mapa_p2, 5, 'P');
-    posicionar_navio(nav_mapa_p2, 4, 'E');
-    posicionar_navio(nav_mapa_p2, 3, 'C');
-    posicionar_navio(nav_mapa_p2, 2, 'S');
+    seed1 = rand();
+    posicionar_navio(nav_mapa_p2, 5, 'P', seed1);
+    seed1 = rand();
+    posicionar_navio(nav_mapa_p2, 4, 'E', seed1);
+    seed1 = rand();
+    posicionar_navio(nav_mapa_p2, 4, 'E', seed1);
+    seed1 = rand();
+    posicionar_navio(nav_mapa_p2, 3, 'C', seed1);
+    seed1 = rand();
+    posicionar_navio(nav_mapa_p2, 3, 'C', seed1);
+    seed1 = rand();
+    posicionar_navio(nav_mapa_p2, 2, 'S', seed1);
+    seed1 = rand();
+    posicionar_navio(nav_mapa_p2, 2, 'S', seed1);
+    seed1 = rand();
+    posicionar_navio(nav_mapa_p2, 2, 'S', seed1);
 
+    imprimir_mapa(nav_mapa_p1);
+    imprimir_mapa(nav_mapa_p2);
+    
     imprimir_instrucoes();
 
     obter_nomes(nome_p1, nome_p2);
@@ -88,20 +117,29 @@ void montar_mapas(char mostrar_mapa_p1[][TAM_MAPA], char mostrar_mapa_p2[][TAM_M
         }
 }
 
-void posicionar_navio(char mapa[][TAM_MAPA], short int tam_navio, char navio)
+void posicionar_navio(char mapa[][TAM_MAPA], short int tam_navio, char navio, unsigned int seed2)
 {
     /* Randomizacao */
-    time_t t;
-    srand((unsigned)time(&t));
-
-    short int j, ran_x, ran_y, check;
+    srand((unsigned)seed2);
+    short int j, ran_x, ran_y, ran_direcao,check;
 
     do{
         check = 0;
-        ran_x = rand() % (TAM_MAPA - tam_navio);
-        ran_y = rand() % TAM_MAPA;
-        for(j = 0; j < tam_navio && mapa[ran_y][j + ran_x] == '~'; ++j, ++check)
-            mapa[ran_y][j + ran_x] = navio;
+        ran_direcao = rand() % 2;
+        if(ran_direcao == 0)
+        {
+          ran_x = rand() % (TAM_MAPA - tam_navio);
+          ran_y = rand() % TAM_MAPA;
+          for(j = 0; j < tam_navio && mapa[ran_y][j + ran_x] == '~'; ++j, ++check)
+              mapa[ran_y][j + ran_x] = navio;
+        }
+        else
+        {
+          ran_x = rand() % TAM_MAPA;
+          ran_y = rand() % (TAM_MAPA - tam_navio);
+          for(j = 0; j < tam_navio && mapa[ran_y + j][ran_x] == '~'; ++j, ++check)
+              mapa[ran_y + j][ran_x] = navio;
+        }
     }while(check != tam_navio);
 }
 
